380f9739e7b5d64c2c31f1b4e21bb62f646e6845
Karanvir Singh <97867019+karansahi001@users.noreply.github.com>
2022-05-14 19:40:20 -0400

    clear history button add

--- src/components/Responses.tsx
+++ src/components/Responses.tsx
@@ -1,21 +1,32 @@
 import React from 'react'
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Card from './Card'
 import { RootState } from '../redux/store'
+import { promptsClear } from '../redux/allPrompts';
 
 const Responses: React.FC = () => {
   // getting state of "allPrompts" from redux 
   const { allPrompts } = useSelector((state: RootState) => state.allPromptser);
+  const dispatch = useDispatch();
+  const handleHistory = (e: React.MouseEvent): void => {
+      dispatch(promptsClear());
+  }
 
   return (
     <div className="container mt-5">
       <div className="row justify-content-center mb-4">
         <div className="col-8">
-          <h2 className="display-6">Responses</h2>
+          <h2 className="display-5">Responses</h2>
         </div>
         <div className="col-8 mt-2">
           <p className="lead fs-6">NOTE: I reply <span className="fw-bold">"Unknown"</span> to the questions I don't know</p>
         </div>
+          
+        {allPrompts.length > 0 ?
+          <div className="col-8 text-end">
+          <button onClick={handleHistory} className="btn shadow-none btn-danger btn-sm">clear history</button>
+        </div>:
+        null}
       </div>
       <div className="row justify-content-center">
         <div className="col-8">
@@ -25,7 +36,9 @@ const Responses: React.FC = () => {
               allPrompts.map((res: any, key: number) => {
                 return <Card key={key} prompt={res.prompt} response={res.response} />
               }) :
-              <h2 className="text-center"><span className="badge bg-dark">No questions asked yet</span></h2>
+             
+                <h2 className="text-center"><span className="badge bg-dark">No questions asked yet</span></h2>
+              
           }
         </div>
       </div>
