a3d9522b7414f7993fc864e44fc66c099d6bf7b7
Karanvir Singh <97867019+karansahi001@users.noreply.github.com>
2022-05-14 18:57:13 -0400

    Font change

--- src/components/Responses.tsx
+++ src/components/Responses.tsx
@@ -11,21 +11,21 @@ const Responses: React.FC = () => {
     <div className="container mt-5">
       <div className="row justify-content-center mb-4">
         <div className="col-8">
-          <h2 className="display-6 fw-bold">Responses</h2>
+          <h2 className="display-6">Responses</h2>
         </div>
         <div className="col-8 mt-2">
-          <p className="fs-6">NOTE: I reply <span className="fw-bold">"Unknown"</span> to the questions I don't know</p>
+          <p className="lead fs-6">NOTE: I reply <span className="fw-bold">"Unknown"</span> to the questions I don't know</p>
         </div>
       </div>
       <div className="row justify-content-center">
         <div className="col-8">
           {/* Mapping over all user prompts and showing data in Card component*/}
           {
-            allPrompts ?
+            allPrompts.length > 0 ?
               allPrompts.map((res: any, key: number) => {
                 return <Card key={key} prompt={res.prompt} response={res.response} />
               }) :
-              <h2>Waiting</h2>
+              <h2 className="text-center"><span className="badge bg-dark">No questions asked yet</span></h2>
           }
         </div>
       </div>
