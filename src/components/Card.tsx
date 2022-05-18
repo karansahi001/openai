6f73080ee577bc152ddc4d9802340c0b0321d381
Karanvir Singh <97867019+karansahi001@users.noreply.github.com>
2022-05-18 00:24:30 -0400

    dark mode for card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { RootState } from '../redux/store';
 
 interface Props{
     prompt: string,
@@ -6,11 +8,12 @@ interface Props{
 }
 
 const Card: React.FC<Props> = ({prompt, response}) => {
+  const { mode } = useSelector((state: RootState) => state.moder);
   return (
-    <div className="card border border-5 rounded mb-3 py-4 px-3">
+    <div className={`card border border-5 rounded mb-3 py-4 px-3 ${mode == 'dark' ? 'bg-dark text-light' : ''}`}>
         <div className="row align-items-start jutify-content-center ms-4">
           <div className="col-lg-3 col-md-5 col-12">
-            <p className="lead fw-bold text-danger">Question: </p>
+            <p className="lead text-danger">Question: </p>
           </div>
           <div className="col-lg-9 col-md-7 col-12">
             <p className="lead">{prompt}</p>
@@ -18,7 +21,7 @@ const Card: React.FC<Props> = ({prompt, response}) => {
         </div>
         <div className="row align-items-start jutify-content-center ms-4">
           <div className="col-lg-3 col-md-4 col-12">
-          <p className="lead fw-bold text-success">Answer: </p>
+          <p className="lead text-success">Answer: </p>
           </div>
           <div className="col-lg-9 col-md-8 col-12">
           <p className="lead">{response}</p>
