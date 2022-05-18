e4bc69a678646503245f986a38a85aa27ce5a9da
Karanvir Singh <97867019+karansahi001@users.noreply.github.com>
2022-05-18 00:25:59 -0400

    dark mode in redux store

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -2,12 +2,15 @@ import { configureStore } from '@reduxjs/toolkit'
 import promptReducer from './prompt'
 import responseReducer from './response'
 import allPromptsReducer from './allPrompts'
+import modeReducer from './mode'
 
 export const store = configureStore({
   reducer: {
     prompter: promptReducer,
     responser: responseReducer,
     allPromptser: allPromptsReducer,
+    moder: modeReducer,
+
   },
 })
 
